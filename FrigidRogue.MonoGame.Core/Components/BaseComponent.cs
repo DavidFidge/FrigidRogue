a36d53f79a3c635658357aabdd494259596ab517
David Fidge <dfidge@internode.on.net>
2021-12-09 18:52:08 +1030

    Add game provider to base component

--- FrigidRogue.MonoGame.Core/Components/BaseComponent.cs
+++ FrigidRogue.MonoGame.Core/Components/BaseComponent.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using FrigidRogue.MonoGame.Core.Interfaces.Components;
+using MediatR;
 
 using Serilog;
 
@@ -8,5 +9,7 @@ namespace FrigidRogue.MonoGame.Core.Components
     {
         public IMediator Mediator { get; set; }
         public ILogger Logger { get; set; }
+        public IGameProvider GameProvider { get; set; }
+        public IGame Game => GameProvider.Game;
     }
 }
