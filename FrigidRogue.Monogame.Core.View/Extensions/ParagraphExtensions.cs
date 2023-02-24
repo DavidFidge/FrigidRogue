96a2295c183fdc88aad232434be2a8e395ad702e
David Fidge <dfidge@internode.on.net>
2023-02-24 14:47:53 +1030

    Add extension methods for GeonBit paragraph

---
+++ FrigidRogue.Monogame.Core.View/Extensions/ParagraphExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.CompilerServices;
+using GeonBit.UI.Entities;
+
+using MediatR;
+using Microsoft.Xna.Framework;
+
+namespace FrigidRogue.MonoGame.Core.View.Extensions
+{
+    public static class ParagraphExtensions
+    {
+        public static Paragraph NoWrap(this Paragraph paragraph)
+        {
+            paragraph.WrapWords = false;
+
+            return paragraph;
+        }
+        
+        public static Paragraph Wrap(this Paragraph paragraph)
+        {
+            paragraph.WrapWords = true;
+
+            return paragraph;
+        }
+        
+        public static Rectangle GetTextDestRect(this Paragraph paragraph)
+        {
+            paragraph.CalcTextActualRectWithWrap();
+            var paragraphTextRect = paragraph.GetActualDestRect();
+            return paragraphTextRect;
+        }
+        
+        public static Paragraph RecalculateWidth(this Paragraph paragraph)
+        {
+            paragraph.CalcTextActualRectWithWrap();
+            var paragraphTextRect = paragraph.GetActualDestRect();
+            paragraph.Width(paragraphTextRect.Width);
+
+            return paragraph;
+        }
+        
+        public static Paragraph RecalculateWidth(this Paragraph paragraph, int extraRightPadding)
+        {
+            paragraph.RecalculateWidth();
+            paragraph.Width(paragraph.Size.X + extraRightPadding);
+
+            return paragraph;
+        }
+    }
+}
