12cb83bed28794adb1faa1b736face702e768f9d
David Fidge <dfidge@internode.on.net>
2023-03-15 13:39:58 +1030

    Add extension methods for transparent colours

---
+++ FrigidRogue.Monogame.Core.View/Extensions/ColorExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace FrigidRogue.MonoGame.Core.View.Extensions;
+
+public static class ColorExtensions
+{
+    /// <summary>
+    /// Create a color with an alpha component
+    /// </summary>
+    /// <param name="color">Color</param>
+    /// <param name="opacity">0 = transparent, 1 = opaque</param>
+    /// <returns></returns>
+    public static Color WithTransparency(this Color color, float opacity)
+    {
+        return new Color(color, opacity);
+    }
+    
+    /// <summary>
+    /// https://shawnhargreaves.com/blog/premultiplied-alpha.html
+    /// </summary>
+    /// <param name="color"></param>
+    /// <param name="opacity"></param>
+    /// <returns></returns>
+    public static Color WithTransparencyPremultiplied(this Color color, float opacity)
+    {
+        return (color * opacity).WithTransparency(opacity);
+    }
+}
