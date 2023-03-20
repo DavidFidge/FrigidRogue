fb5eb523ef00ac11ad3a676fa6f4bdfa963f8fb4
David Fidge <dfidge@internode.on.net>
2023-03-20 18:31:34 +1030

    Add Path extension to get the next step after a particular point. Add GridViewExtensions to allow printing of a Path on a map.

--- FrigidRogue.MonoGame.Core.Tests/ExtensionsTests/PathExtensionsTests.cs
+++ FrigidRogue.MonoGame.Core.Tests/ExtensionsTests/PathExtensionsTests.cs
@@ -13,34 +13,14 @@ namespace FrigidRogue.MonoGame.Core.Tests.Extensions
     public class PathExtensionsTests : BaseTest
     {
         [TestMethod]
-        public void GetStepAfterPoint_Should_Get_Step_After_Given_Point_1()
+        public void GetStepAfterPoint_Should_Exclude_First_Step()
         {
             // Arrange
             var steps = new[]
             {
-                new Point(0, 0),
                 new Point(1, 1),
-                new Point(2, 2)
-            };
-            
-            var path = new Path(steps);
-
-            // Act
-            var result = path.GetStepAfterPoint(new Point(0, 0));
-            
-            // Assert
-            Assert.AreEqual(new Point(1, 1), result);
-        }
-        
-        [TestMethod]
-        public void GetStepAfterPoint_Should_Get_Step_After_Given_Point_2()
-        {
-            // Arrange
-            var steps = new[]
-            {
-                new Point(0, 0),
-                new Point(1, 1),
-                new Point(2, 2)
+                new Point(2, 2),
+                new Point(3, 3)
             };
             
             var path = new Path(steps);
@@ -49,7 +29,27 @@ namespace FrigidRogue.MonoGame.Core.Tests.Extensions
             var result = path.GetStepAfterPoint(new Point(1, 1));
             
             // Assert
-            Assert.AreEqual(new Point(2, 2), result);
+            Assert.AreEqual(Point.None, result);
+        }
+        
+        [TestMethod]
+        public void GetStepAfterPoint_Should_Get_Step_After_Given_Point()
+        {
+            // Arrange
+            var steps = new[]
+            {
+                new Point(1, 1),
+                new Point(2, 2),
+                new Point(3, 3)
+            };
+            
+            var path = new Path(steps);
+
+            // Act
+            var result = path.GetStepAfterPoint(new Point(2, 2));
+            
+            // Assert
+            Assert.AreEqual(new Point(3, 3), result);
         }
         
         [TestMethod]
@@ -58,15 +58,15 @@ namespace FrigidRogue.MonoGame.Core.Tests.Extensions
             // Arrange
             var steps = new[]
             {
-                new Point(0, 0),
                 new Point(1, 1),
-                new Point(2, 2)
+                new Point(2, 2),
+                new Point(3, 3)
             };
             
             var path = new Path(steps);
 
             // Act
-            var result = path.GetStepAfterPoint(new Point(2, 2));
+            var result = path.GetStepAfterPoint(new Point(3, 3));
             
             // Assert
             Assert.AreEqual(Point.None, result);
@@ -78,13 +78,13 @@ namespace FrigidRogue.MonoGame.Core.Tests.Extensions
             // Arrange
             var steps = new[]
             {
-                new Point(0, 0)
+                new Point(1, 1)
             };
             
             var path = new Path(steps);
 
             // Act
-            var result = path.GetStepAfterPoint(new Point(0, 0));
+            var result = path.GetStepAfterPoint(new Point(1, 1));
             
             // Assert
             Assert.AreEqual(Point.None, result);
@@ -113,18 +113,133 @@ namespace FrigidRogue.MonoGame.Core.Tests.Extensions
             // Arrange
             var steps = new[]
             {
-                new Point(0, 0),
                 new Point(1, 1),
-                new Point(2, 2)
+                new Point(2, 2),
+                new Point(3, 3)
             };
             
             var path = new Path(steps);
 
             // Act
-            var result = path.GetStepAfterPoint(new Point(3, 3));
+            var result = path.GetStepAfterPoint(new Point(4, 4));
             
             // Assert
             Assert.AreEqual(Point.None, result);
         }
+        
+        [TestMethod]
+        public void GetStepAfterPointWithStart_Should_Get_Step_After_Given_Point_1()
+        {
+            // Arrange
+            var steps = new[]
+            {
+                new Point(1, 1),
+                new Point(2, 2),
+                new Point(3, 3)
+            };
+            
+            var path = new Path(steps);
+
+            // Act
+            var result = path.GetStepAfterPointWithStart(new Point(1, 1));
+            
+            // Assert
+            Assert.AreEqual(new Point(2, 2), result);
+        }
+        
+        [TestMethod]
+        public void GetStepAfterPointWithStart_Should_Get_Step_After_Given_Point_2()
+        {
+            // Arrange
+            var steps = new[]
+            {
+                new Point(1, 1),
+                new Point(2, 2),
+                new Point(3, 3)
+            };
+            
+            var path = new Path(steps);
+
+            // Act
+            var result = path.GetStepAfterPointWithStart(new Point(2, 2));
+            
+            // Assert
+            Assert.AreEqual(new Point(3, 3), result);
+        }
+        
+        [TestMethod]
+        public void GetStepAfterPointWithStart_Should_Return_None_If_No_Step_After_Given_Point_1()
+        {
+            // Arrange
+            var steps = new[]
+            {
+                new Point(1, 1),
+                new Point(2, 2),
+                new Point(3, 3)
+            };
+            
+            var path = new Path(steps);
+
+            // Act
+            var result = path.GetStepAfterPointWithStart(new Point(3, 3));
+            
+            // Assert
+            Assert.AreEqual(Point.None, result);
+        }
+        
+        [TestMethod]
+        public void GetStepAfterPointWithStart_Should_Return_None_If_No_Step_After_Given_Point_2()
+        {
+            // Arrange
+            var steps = new[]
+            {
+                new Point(1, 1)
+            };
+            
+            var path = new Path(steps);
+
+            // Act
+            var result = path.GetStepAfterPointWithStart(new Point(1, 1));
+            
+            // Assert
+            Assert.AreEqual(Point.None, result);
+        }
+        
+        [TestMethod]
+        public void GetStepAfterPointWithStart_Should_Return_None_If_No_Steps_In_Path()
+        {
+            // Arrange
+            var steps = new Point[]
+            {
+            };
+            
+            var path = new Path(steps);
+
+            // Act
+            var result = path.GetStepAfterPointWithStart(new Point(0, 0));
+            
+            // Assert
+            Assert.AreEqual(Point.None, result);
+        }
+        
+        [TestMethod]
+        public void GetStepAfterPointWithStart_Should_Return_None_If_Given_Point_Not_In_List_Of_Steps()
+        {
+            // Arrange
+            var steps = new[]
+            {
+                new Point(1, 1),
+                new Point(2, 2),
+                new Point(3, 3)
+            };
+            
+            var path = new Path(steps);
+
+            // Act
+            var result = path.GetStepAfterPointWithStart(new Point(4, 4));
+            
+            // Assert
+            Assert.AreEqual(Point.None, result);
+        }       
     }
 }
--- FrigidRogue.MonoGame.Core/Extensions/PathExtensions.cs
+++ FrigidRogue.MonoGame.Core/Extensions/PathExtensions.cs
@@ -9,6 +9,9 @@ namespace FrigidRogue.MonoGame.Core.Extensions
         {
             var enumerator = path.Steps.GetEnumerator();
 
+            if (!enumerator.MoveNext())
+                return Point.None;
+
             while (true)
             {
                 if (enumerator.Current == point)
@@ -22,5 +25,28 @@ namespace FrigidRogue.MonoGame.Core.Extensions
                     return Point.None;
             }
         }
+        
+        public static Point GetStepAfterPointWithStart(this Path path, Point point)
+        {
+            var enumerator = path.StepsWithStart.GetEnumerator();
+
+            if (!enumerator.MoveNext())
+                return Point.None;
+
+            while (true)
+            {
+                if (enumerator.Current == point)
+                {
+                    var hasNextValue = enumerator.MoveNext();
+
+                    return hasNextValue ? enumerator.Current : Point.None;
+                }
+
+                if (!enumerator.MoveNext())
+                    return Point.None;
+            }
+        }
+        
+        
     }
 }
