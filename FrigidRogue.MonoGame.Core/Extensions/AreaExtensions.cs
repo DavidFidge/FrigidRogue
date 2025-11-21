1403bcd6464da34c05dd191753f576c74810b940
David Fidge <dfidge@gmail.com>
2025-11-21 18:16:08 +1030

    progress on canteen  / hole in the wall

---
+++ FrigidRogue.MonoGame.Core/Extensions/AreaExtensions.cs
@@ -0,0 +1,20 @@
+using SadRogue.Primitives;
+
+namespace FrigidRogue.MonoGame.Core.Extensions
+{
+    public static class AreaExtensions
+    {
+        public static Point GetGeometricCentroiod(this Area area, AdjacencyRule adjacencyRule)
+        {
+            var perimeterPoints = area.PerimeterPositions(adjacencyRule).ToList();
+
+            // Compute the geometric centroid of the perimeter points (average X and Y)
+            var avgX = perimeterPoints.Average(p => p.X);
+            var avgY = perimeterPoints.Average(p => p.Y);
+
+            var point = new Point((int)avgX, (int)avgY);
+
+            return point;
+        }
+    }
+}
