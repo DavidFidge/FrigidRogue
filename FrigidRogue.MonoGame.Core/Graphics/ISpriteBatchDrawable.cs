add6b2432eb4a2c0de4771651217eebcc94ce3d0
David Fidge <dfidge@gmail.com>
2026-02-13 17:45:34 +1030

    skeleton for health bar

--- FrigidRogue.MonoGame.Core/Graphics/ISpriteBatchDrawable.cs
+++ FrigidRogue.MonoGame.Core/Graphics/ISpriteBatchDrawable.cs
@@ -5,6 +5,11 @@ namespace FrigidRogue.MonoGame.Core.Graphics
     public interface ISpriteBatchDrawable
     {
         public bool IsVisible { get; set; }
+
+        // A single texture atlas is used for this draw, making it very quick
         void SpriteBatchDraw(SpriteBatch spriteBatch);
+
+        // This one allows tiles to draw any additional overlays like health bars
+        void OverlayDraw(SpriteBatch spriteBatch);
     }
 }
