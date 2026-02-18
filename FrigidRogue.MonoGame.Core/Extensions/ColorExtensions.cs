0f015079574797595a157fc5f1be349fffbb6154
David Fidge <dfidge@gmail.com>
2026-02-19 07:47:35 +1030

    Healthbar (#22)
    * skeleton for health bar
    
    * Add a health bar on bottom of units

---
+++ FrigidRogue.MonoGame.Core/Extensions/ColorExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FrigidRogue.MonoGame.Core.Extensions
+{
+    public static class ColorExtensions
+    {
+        public static Color ToXna(this SadRogue.Primitives.Color c)
+        {
+            return new Color(c.R, c.G, c.B, c.A);
+        }
+    }
+}
