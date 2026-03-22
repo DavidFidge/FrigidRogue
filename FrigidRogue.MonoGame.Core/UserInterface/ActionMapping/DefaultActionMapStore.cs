b78df78ae672fb77fa6c4477827c519722b800cf
David Fidge <dfidge@gmail.com>
2026-03-23 08:26:48 +1030

    dismiss bubblr thought with space or esc

--- FrigidRogue.MonoGame.Core/UserInterface/ActionMapping/DefaultActionMapStore.cs
+++ FrigidRogue.MonoGame.Core/UserInterface/ActionMapping/DefaultActionMapStore.cs
@@ -9,14 +9,20 @@ namespace FrigidRogue.MonoGame.Core.UserInterface
 
         public DefaultActionMapStore()
         {
-            _keyMap = AppDomain.CurrentDomain
+            var keymapTypes = AppDomain.CurrentDomain
                 .GetAssemblies()
                 .AsParallel()
                 .SelectMany(a => a.GetTypes())
                 .SelectMany(t => t.GetAttributes<ActionMapAttribute>())
-                .Where(t => t != null)
-                .ToDictionary(t => t.Name, t => new KeyCombination(t.DefaultKey, t.DefaultKeyboardModifier));
+                .Where(t => t != null);
+
+            if (keymapTypes.Any(km => string.IsNullOrEmpty(km.Name)))
+            {
+                throw new Exception($"There are ActionMapAttributes that do not have a Name.");
             }
+
+            _keyMap = keymapTypes.ToDictionary(t => t.Name, t => new KeyCombination(t.DefaultKey, t.DefaultKeyboardModifier));
+        }
 
         public Dictionary<string, KeyCombination> GetKeyMap()
         {
